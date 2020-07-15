24d51b79aa69b4ddbc482f5c35475e482aca061e
kawinthip <kawinthipt60@nu.ac.th>
2020-07-16 02:22:50 +0700

    folder perpared

--- server/src/app.js
+++ server/src/app.js
@@ -1,49 +1,55 @@
 let express = require('express')
-let bodyParser= require('body-parser')
+let bodyParser = require('body-parser')
 
-const app = express()
+const app = express() 
+const {sequelize} = require('./models')
+const config = require('./config/config')
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({extended: true}))
 
-app.get('/status',function (req, res){
+require('./routes')(app)
+
+let port = process.env.PORT || config.port
+sequelize.sync({force: false}).then(() => {
+    app.listen(port, function () {
+        console.log('Server running on ' + port)
+    })
+})
+
+ 
+app.get('/status', function (req, res){
     res.send('Hello nodejs server')
 })
 
-app.get('/hello/:person', function(req,res) {
-    console.log('hello -'+ req.params.person)
-    res.send('sey hello with '+ req.params.person)
+app.get('/hello/:person', function (req,res) {
+    console.log('hello - ' + req.params.person)
+    res.send('sey hello with ' + req.params.person)
 })
 
 // get user by id
-app.get('/user/:userId', function(req, res) 
-{res.send('ดูข้อมูลผู้ใช้งาน : '+ req.params.userId)
+app.get('/user/:userId', function (req, res) {
+    res.send('ดูข้อมูลผู้ใช้งาน: '+ req.params.userId)
 })
 
 // get all user
-app.get('/users', function(req, res) {
+app.get('/users', function (req, res) {
     res.send('เรียกข้อมูลผู้ใช้งานทั้งหมด')
 })
-
-let port = 8081
-
-app.listen(port, function () {
-    console.log('server running on '+ port)
-})
-
+ 
 // create user
-app.post('/user/', function(req, res) {
-res.send('ทําการสร้างผู้ใช้งาน: '+ JSON.stringify(req.body))
+app.post('/user/', function (req, res) {
+    res.send('ทำการสร้างผู้ใช้งาน: ' + JSON.stringify(req.body))
 })
 
 // edit user
-app.put('/user/:userId', function(req, res) {
-res.send('ทําการแก้ไขผุ้ใช้งาน: '+ req.params.userId+ ' : '+
-JSON.stringify(req.body))
+app.put('/user/:userId', function (req, res) {
+    res.send('ทำการแก้ไขผุ้ใช้งาน: ' + req.params.userId + ' : ' +
+    JSON.stringify(req.body))
 })
 
 // delete user
-app.delete('/user/:userId', function(req, res) {
-res.send('ทําการลบผุ้ใช้งาน: '+ req.params.userId+ ' : '+ 
-JSON.stringify(req.body))
+app.delete('/user/:userId', function (req, res) {
+    res.send('ทำการลบผู้ใช้งาน: ' + req.params.userId + ' : ' +
+    JSON.stringify(req.body))
 })
